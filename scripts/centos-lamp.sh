488bbc88a1cfdae37c7d57a66bae1b99f0be2dbf
Michael Kuehn <mkuehn10@gmail.com>
2021-12-30 14:39:36 -0500

    Updated LAMP script with Github files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -28,5 +28,8 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 # Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/mkuehn10/vagrant/main/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/mkuehn10/vagrant/main/files/info.php
 
 service httpd restart
